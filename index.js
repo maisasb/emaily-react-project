8f062763fa86ac2c89222fac11bd7466b92e300c
Maisa <maisasb15@gmail.com>
2019-10-05 12:51:46 -0300

    changed greeting

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({ bye: 'buddy' });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 //http://localhost:5000/
+//https://git.heroku.com/boiling-temple-05045.git
+//https://boiling-temple-05045.herokuapp.com/
