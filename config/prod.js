d17bde707ffa25e7b63ec499a08ebabaddf40fa1
Maisa <maisasb15@gmail.com>
2019-10-08 19:55:44 -0300

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+// prod.js - production keys here
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mogoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
+
+// mongodb+srv://maisa:efMDiekWLMM6pJN3@emaily-prod-qg8rx.mongodb.net/anything?retryWrites=true&w=majority
+// 411138880394-jet683fanm2am0hbt6eibd78bf2t1qae.apps.googleusercontent.com
+// EUo5blOfWy14FYNGUquOaRb-
